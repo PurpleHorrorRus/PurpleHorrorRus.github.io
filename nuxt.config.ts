020eb628927da491b5a22d91b73075bdc4dcda2d
PurpleHorrorRus <purplehorrorrus@gmail.com>
2022-01-28 22:38:51 +0800

    Add meta to config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -12,6 +12,15 @@ const svgo = svgLoader({
 // https://v3.nuxtjs.org/docs/directory-structure/nuxt.config
 export default defineNuxtConfig({
     meta: {
+        meta: [
+            {
+                charset: "utf-8",
+            },
+            {
+                name: "viewport",
+                content: "width=device-width, initial-scale=1"
+            }
+        ],
         link: [
             {
                 rel: "preconnect",
