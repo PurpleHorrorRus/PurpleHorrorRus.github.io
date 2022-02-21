20373eab6154ebc101c8d526810e64bdf7c3ba4f
PurpleHorrorRus <purplehorrorrus@gmail.com>
2022-02-21 17:45:20 +0800

    Update version

--- components/Meridius/Download.vue
+++ components/Meridius/Download.vue
@@ -15,7 +15,7 @@
 import WindowsIcon from "~/assets/icons/windows.svg";
 import LinuxIcon from "~/assets/icons/linux.svg";
 
-const version = "2.3.2";
+const version = "2.3.3";
 
 export default {
     components: {
