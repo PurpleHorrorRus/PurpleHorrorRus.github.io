d1182855d5cf1e8d30eb8a91c944b1c997f84753
PurpleHorrorRus <purplehorrorrus@gmail.com>
2022-03-08 13:00:49 +0800

    Improve token page

---
+++ components/Buttons/SolidButton.vue
@@ -0,0 +1,47 @@
+<template>
+    <div class="solid-button" :class="{ clickable: !disabled }">
+        <slot />
+    </div>
+</template>
+
+<script>
+export default {
+    props: {
+        disabled: {
+            type: Boolean,
+            required: false,
+            default: false
+        }
+    }
+};
+</script>
+
+<style lang="scss">
+.solid-button {
+    display: flex;
+    justify-self: center;
+    align-items: center;
+
+    width: max-content;
+    height: 40px;
+
+    padding: 10px;
+
+    background: var(--secondary);
+    border-radius: 4px;
+
+    .icon {
+        width: 24px;
+    }
+
+    &.clickable {
+        cursor: pointer;
+
+        &:hover {
+            .icon {
+                fill: #ffffff;
+            }
+        }
+    }
+}
+</style>
--- pages/token.vue
+++ pages/token.vue
@@ -5,28 +5,68 @@
                 Ваш Token находится в этом поле. Скопируйте его и вставьте в приложение.
             </span>
             
-            <span id="token-page-container-token" v-text="token" />
+            <div id="token-page-container-field">
+                <span id="token-page-container-field-token" v-text="token" />
+                <SolidButton v-tooltip="tooltip" :disabled="copied" @click.native="copy">
+                    <CopyIcon v-if="!copied" class="icon" />
+                    <CheckIcon v-else class="icon" />
+                </SolidButton>
+            </div>
         </div>
     </div>
 </template>
 
 <script>
+import SolidButton from "~/components/Buttons/SolidButton";
+
+import CopyIcon from "~/assets/icons/token/copy.svg";
+import CheckIcon from "~/assets/icons/token/check.svg";
+
 const tokenRegex = [
-    /access_token=(.*?)&/
+    /access_token=([^&]+)/
 ];
 
 export default {
+    components: { 
+        SolidButton,
+
+        CopyIcon,
+        CheckIcon
+    },
     layout: "empty",
     data: () => ({
-        token: ""
+        token: "",
+        copied: false
     }),
+    computed: {
+        tooltip() {
+            return !this.copied 
+                ? "Скопировать" 
+                : "Скопировано";
+        }
+    },
     mounted() {
-        if (window.location.hash) {
-            for (const regex of tokenRegex) {
-                if (regex.test(window.location.hash)) {
-                    this.token = window.location.hash.match(regex)[1];
-                    break;
-                }
+        for (const regex of tokenRegex) {
+            if (regex.test(window.location.href)) {
+                this.token = window.location.href.match(regex)[1];
+                break;
+            }
+
+            if (regex.test(window.location.hash)) {
+                this.token = window.location.hash.match(regex)[1];
+                break;
+            }
+        }
+    },
+    methods: {
+        async copy() {
+            if (this.token.length > 0 && !this.copied) {
+                navigator.clipboard.writeText(this.token);
+                this.copied = true;
+                
+                setTimeout(() => {
+                    this.copied = false;
+                }, 4000);
             }
         }
     }
@@ -58,16 +98,24 @@ export default {
         background: #333333;
         border-radius: 4px;
 
-        &-token {
+        &-field {
+            display: flex;
+            align-items: center;
+            column-gap: 10px;
+
             width: 100%;
-            
-            padding: 10px;
 
-            background: var(--backdrop);
-            border: 2px solid #ffffff;
-            border-radius: 4px;
+            &-token {
+                width: 100%;
+                
+                padding: 10px;
 
-            user-select: text;
+                background: var(--backdrop);
+                border: 2px solid #ffffff;
+                border-radius: 4px;
+
+                user-select: text;
+            }
         }
     }
 }
